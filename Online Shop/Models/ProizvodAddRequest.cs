a124a8aa03af2b8c6562ee69ff88ea840b9377bc
owl_coder <danijel.xda@gmail.com>
2023-06-16 14:24:41 +0200

    add request class model

---
+++ Online Shop/Models/ProizvodAddRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shop.Models
+{
+    public class ProizvodAddRequest
+    {
+        [Required]
+        public string Naziv { get; set; }
+
+        [Required]
+        public double Cena { get; set; }
+
+        [Required]
+        public double Kolicina { get; set; }
+
+        [Required]
+        public string Opis { get; set; }
+
+        [Required]
+        public string Slika { get; set; }
+
+        [Required]
+        public string Grad { get; set; }
+    }
+}
