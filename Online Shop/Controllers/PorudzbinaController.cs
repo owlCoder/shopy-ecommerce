96e65b28ecab72bcd1c85a46575cf541a41e2c3b
owl_coder <danijel.xda@gmail.com>
2023-06-19 23:17:08 +0200

    add new order request

---
+++ Online Shop/Controllers/PorudzbinaController.cs
@@ -0,0 +1,21 @@
+﻿using Online_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Online_Shop.Controllers
+{
+    [RoutePrefix("api/orders")]
+    public class PorudzbinaController : ApiController
+    {
+        [HttpPost]
+        [Route("KreiranjePorudzbine")]
+        public string KreirajPorudzbinu(SingleIdRequest zahtev)
+        {
+            // prima se Id proizvoda koji se narucuje kao i kolicina koja je trazena
+        }
+    }
+}
---
+++ Online Shop/Models/OrderRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shop.Models
+{
+    public class OrderRequest
+    {
+        [Required]
+        public int Id{ get; set; }
+        [Required]
+        public double Kolicina { get; set; }
+    }
+}
