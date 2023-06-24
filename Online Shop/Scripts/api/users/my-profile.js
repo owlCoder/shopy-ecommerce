0ca9c81808790b471c00070f466bdccb4a2d6cb0
owl_coder <danijel.xda@gmail.com>
2023-06-24 10:10:35 +0200

    Update user profile to more intuitive

---
+++ Online Shop/Scripts/api/users/my-profile.js
@@ -0,0 +1,75 @@
+﻿// ako je ulogovan, prebaci ga na index - slucaj da ide na back opciju
+$.ajax({
+    url: "/api/auth/Ulogovan",
+    type: "GET",
+    async: false,
+    cache: false,
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function (response) {
+        if (response != null) {
+            if (JSON.parse(response).Kod !== 0) // korisnik je ulogovan, ne moze da se registruje
+            {
+                window.location.href = "Index.html";
+            }
+        }
+    }
+});
+
+// ako je ulogovan, prebaci prikazi meni u zavisnoti od uloge
+$.ajax({
+    url: "/api/auth/AuthKorisnik",
+    type: "GET",
+    async: false,
+    cache: false,
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function (response) {
+        var data = JSON.parse(response);
+        if (data.KorisnickoIme !== "") {
+            // prikazi podatke i promeni meni
+            // sakri dugmice za login i register
+            $(function () {
+                $("#pl").remove();
+
+                // prikazi meni za nalog
+                $('#poruka').text("Dobrodošli nazad, " + data.KorisnickoIme);
+
+                // Prikazi samo dugmice vezane za korisnika koji ima datu ulogu
+                var uloga = data.Uloga;
+
+                if (uloga === 0) {
+                    // u pitanju je kupac
+                    $("#prodavacmeni").remove();
+                    $("#adminmeni").remove();
+                    $("#uloga").text("Kupac");
+                }
+                else if (uloga === 1) {
+                    // u pitanju je prodavac
+                    $("#kupacmeni").remove();
+                    $("#adminmeni").remove();
+                    $("#uloga").text("Prodavac");
+                }
+                else if (uloga === 2) {
+                    // u pitanju je administrator
+                    $("#prodavacmeni").remove();
+                    $("#kupacmeni").remove();
+                    $("#uloga").text("Administrator");
+                }
+
+                $("body").removeClass("d-none");
+
+                return false;
+            });
+        }
+        else {
+            $(function () {
+                // korisnik nije ulogovan - prikazi login i register
+                $("#pl").show();
+
+                // sakri meni za nalog
+                $('#profil').addClass('d-none');
+            });
+        }
+    }
+});
